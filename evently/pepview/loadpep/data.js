f439e4f26a504c97e4e4a704897359e97de5ee02
Nathaniel Domingo <niel.domingo@gmail.com>
2011-06-24 13:43:46 +0800

    added title to pep view

--- evently/pepview/loadpep/data.js
+++ evently/pepview/loadpep/data.js
@@ -2,5 +2,7 @@ function (data) {
   var html = data.rows[0].value['html'];
   // remove internal links
   html = html.replace(/href="(?!http:)[^"]+"/g, '');
-  return {'html': html};
+  var title = data.rows[0].value['title'];
+  var pepnum = data.rows[0].value['num'];
+  return {'html': html, 'title': title, 'pepnum': pepnum};
 };
